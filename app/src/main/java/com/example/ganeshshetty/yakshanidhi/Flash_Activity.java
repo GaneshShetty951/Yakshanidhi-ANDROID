1228f8d489f3df6b4ede3a7f9e92a4fdc76c9213
GANESH SHETTY <ganesh.ellipsonic@gmail.com>
2017-02-04 20:32:07 +0530

    splash screen working

--- app/src/main/java/com/example/ganeshshetty/yakshanidhi/Flash_Activity.java
+++ app/src/main/java/com/example/ganeshshetty/yakshanidhi/Flash_Activity.java
@@ -2,20 +2,34 @@ package com.example.ganeshshetty.yakshanidhi;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Flash_Activity extends Activity {
-
+    private int SPLASH_TIME_OUT=3000;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_);
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Intent mainIntent=new Intent(this,MainActivity.class);
-        startActivity(mainIntent);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent mainIntent=new Intent(Flash_Activity.this,MainActivity.class);
+                startActivity(mainIntent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
 }
